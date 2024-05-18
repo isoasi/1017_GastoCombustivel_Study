0d668872855a62a1e89f5e3de9d437ff399bc686
Paola <Paola@DESKTOP-L53JRFL>
2024-05-17 23:09:28 -0300

    Adicionar arquivos de projeto.

---
+++ 1017_gastoCombustivel_Study/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double tempo, velocidadeMedia, total;
+
+        // Ler o tempo/horas gasto na viagem e a velocidade média (em km/h)
+        tempo = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        velocidadeMedia = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        // Calcular a distância percorrida / Gasto por km do carro = quantidade de litros necessária
+        total = (tempo * velocidadeMedia) / 12;
+
+
+        Console.WriteLine(total.ToString("F3", CultureInfo.InvariantCulture));
+
+        Console.Read();
+
+    }
+
+}
